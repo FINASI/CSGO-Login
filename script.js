ad72978ca82c64aae5323951ad36c52eb78fc83d
Leonardo Finasi <69171638+FINASI@users.noreply.github.com>
2022-06-15 18:32:11 -0300

    Add files via upload
    CSGO login screen inspired by Riot Games login design.
    Used: HTML, CSS, JavaScript

---
+++ script.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login__button');
+
+const handleFocus = ({ target }) => {
+  const span = target.previousElementSibling;
+  span.classList.add('span-active');
+}
+
+const handleFocusOut = ({ target }) => {
+  if (target.value === '') {
+    const span = target.previousElementSibling;
+    span.classList.remove('span-active');
+  }
+}
+
+
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
+inputs.forEach((input) => input.addEventListener('input', handleChange));
